b0c1bf927c70464ae0a8ef22b0bd259842742eec
Theia\Ty <ty@imatest.com>
2019-04-10 16:48:10 -0600

    Loading the embedded font on Linux

--- app/main.cpp
+++ app/main.cpp
@@ -3,16 +3,22 @@
 #include <QFile>
 #include <QProcess>
 #include <QTextStream>
+#include <QFontDatabase>
+#include <QFont>
 using namespace std;
 
 
 int main(int argc, char *argv[])
 {
     QFile   out;
-
     out.open(stdout, QIODevice::ReadOnly);
-    QApplication a(argc, argv);
 
+    QApplication a(argc, argv);
+#ifdef __linux
+    // On Linux, we're statically building QT so we must use an embedded font
+    QFontDatabase::addApplicationFont(":/fonts/DejaVuSans.ttf");
+    a.setFont(QFont("DejaVue", 11, QFont::Normal, false));
+#endif
     OperatorConsoleDialog w(nullptr, &out);
 
     w.show();
