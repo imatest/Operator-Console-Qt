fe9c5d139ad045cbc023b76daaef1be114042900
Theia\Ty <ty@imatest.com>
2019-04-26 14:11:04 -0600

    Fixing the order of Mathworks headers so we no longer get linker errors on Windows.

--- app/criticalmwarray.h
+++ app/criticalmwarray.h
@@ -2,7 +2,13 @@
 #define CRITICALMWARRAY_H
 
 #include <QMutex>
+#include "imatestdefines.h"
+IMA_WARNING_PUSH
+IMA_DISABLE_UNUSED_PARAM_WARNING
+#include "mclmcrrt.h"
 #include "mclcppclass.h"
+
+IMA_WARNING_POP
 
 ///
 /// a mwArray wrapper-class where all access (read or write) to the mwArray is protected by critical sections
